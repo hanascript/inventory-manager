dccfc39b53d3fb509622b5b161f3ab960c8a9e55
Nathan Marcellous <taboodog@hotmail.com>
2024-09-16 15:08:45 -0700

    feat: updated actions & added drawer

--- lib/safe-action.ts
+++ lib/safe-action.ts
@@ -1,3 +1,15 @@
-import { createSafeActionClient } from "next-safe-action";
+import { createSafeActionClient } from 'next-safe-action';
 
-export const actionClient = createSafeActionClient();
+export const actionClient = createSafeActionClient({
+  // Can also be an async function.
+  handleServerError(e, utils) {
+    // You can access these properties inside the `utils` object.
+    const { clientInput, bindArgsClientInputs, metadata, ctx } = utils;
+
+    // Log to console.
+    console.error('Action error:', e.message);
+
+    // Return generic message
+    return e.message;
+  },
+});
